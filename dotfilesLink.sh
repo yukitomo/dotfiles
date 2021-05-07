86958ed78774d82e110df6321fb8ef600b4a4475
Yuki Tomo <26.yuki.tomo@gmail.com>
2021-05-07 17:15:36 +0900

    fix synbolic link

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -2,7 +2,6 @@
 
 # zsh
 ln -sf ~/dotfiles/zshrc ~/.zprezto/runcoms/zshrc
-ln -sf ~/dotfiles/zshrc ~/.zprezto/runcoms/zshrc
 ln -sf ~/dotfiles/prompt_tomo_setup ~/.zprezto/modules/prompt/functions/prompt_tomo_setup
 
 # tmux
@@ -10,3 +9,6 @@ ln -sf ~/dotfiles/.tmux.conf  ~/.tmux.conf
 
 # git
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
+
+# karabiner-elements
+ln -sf /Users/tomo/Dropbox/karabiner ~/.config/karabiner
